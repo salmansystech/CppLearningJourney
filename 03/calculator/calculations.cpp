ab223137655aa00937b9125af6713a9cc6ed430d
Salman khan <salmank.fin@gmail.com>
2024-11-01 17:24:56 +0200

    calculator
     Learn to use function pointers.

---
+++ 03/calculator/calculations.cpp
@@ -0,0 +1,26 @@
+// Note that there is no need for "using namespace std",
+// since no C++ standard libraries are used.
+#include <cmath>
+
+double exponentiation(double base, double exp) {
+    return pow(base, exp);
+}
+
+double addition(double left, double right) {
+    return left + right;
+}
+
+
+double subtraction(double left, double right) {
+    return left - right;
+}
+
+
+double multiplication(double left, double right) {
+    return left * right;
+}
+
+
+double division(double left, double right) {
+    return left / right;
+}
